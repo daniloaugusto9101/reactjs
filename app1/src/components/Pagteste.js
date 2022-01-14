bf3cccbd6015b68b842cae60300f798478cace91
daniloaugusto9101 <daniloaugusto9101@gmail.com>
2022-01-14 04:58:11 -0300

    BKP Arquivos

---
+++ app1/src/components/Pagteste.js
@@ -0,0 +1,8 @@
+function Pagteste(){
+  return(
+    <div>
+      <p>tedtes asd asd ad as da </p>
+    </div>
+  )
+}
+export default Pagteste
---
+++ app1/src/components/pages/Sobre.js
@@ -0,0 +1,9 @@
+function Sobre(){
+  return(
+    <div>
+        <h1>Sobre</h1>
+        <p>Pagina Sobre</p>
+    </div>
+  )
+}
+export default Sobre
