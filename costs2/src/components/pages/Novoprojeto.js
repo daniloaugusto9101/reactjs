b111300a048821808fae0fb90ba72b59e4a8059d
daniloaugusto9101 <daniloaugusto9101@gmail.com>
2022-02-21 06:12:35 -0300

    Select concluido

--- costs2/src/components/pages/Novoprojeto.js
+++ costs2/src/components/pages/Novoprojeto.js
@@ -1,13 +1,37 @@
+import { useNavigate } from 'react-router-dom'
+
 import ProjectForm from "../project/ProjectForm";
 
 import styles from './NovoProjeto.module.css';
 
 function NovoProjeto() {
+
+    const history = useNavigate();
+
+    function createPost(project) {
+        project.cost = 0;
+        project.services = [];
+
+        fetch("http://localhost:5000/projetos", {
+            method: "POST",
+            headers: {
+                "Content-type": "aplication/json",
+            },
+            body: JSON.stringify(project),
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                console.log(data)
+                history("/projetos", { message: "Projeto criado com sucesso!" })
+            })
+            .catch(err => console.log(err))
+    }
+
     return (
         <div className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
-            <ProjectForm btnText="Criar projetos"/>
+            <ProjectForm handleSubmit={createPost} btnText="Criar projetos" />
         </div>
     )
 }
